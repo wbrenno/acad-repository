53b6ea773342cf098a356d2679098df912c44b1b
Allan Augusto <137811222+AllanAugusto1@users.noreply.github.com>
2023-10-30 22:35:24 -0300

    Update RendimentoEscolarFakeDB.java

--- src/uniderp/poo/escola/FakeDB/RendimentoEscolarFakeDB.java
+++ src/uniderp/poo/escola/FakeDB/RendimentoEscolarFakeDB.java
@@ -9,13 +9,13 @@ import uniderp.poo.escola.Dominio.Turma;
 
 public class RendimentoEscolarFakeDB extends BaseGenericaFakeDB<RendimentoEscolar>{
 
+    private AlunoFakeDB alunoDB;
+    private NotaTrabalhoFakeDB trabsDB;
+    private TurmaFakeDB turmaDB;
+
     @Override
     protected void CarregarDados() {
 
-     AlunoFakeDB alunoDB = new AlunoFakeDB();
-     NotaTrabalhoFakeDB trabsDB = new NotaTrabalhoFakeDB();
-     TurmaFakeDB turmaDB = new TurmaFakeDB(); 
-        
         ArrayList<Aluno> alunos = alunoDB.getTabela();
 
         ArrayList<Turma> turmas = turmaDB.getTabela();
@@ -25,28 +25,76 @@ public class RendimentoEscolarFakeDB extends BaseGenericaFakeDB<RendimentoEscola
         this.tabela = new ArrayList<RendimentoEscolar>();
 
         this.tabela.add(new RendimentoEscolar(alunos.get(0), turmas.get(0), 6.0, 6.0, trabs.get(0), 6.0, 6.0));
-        this.tabela.add(new RendimentoEscolar(alunos.get(0), turmas.get(0), 7.0, 6.0, trabs.get(1), 7.0, 6.5));
+        this.tabela.add(new RendimentoEscolar(alunos.get(0), turmas.get(0), 7.0, 6.0, trabs.get(1), 7.0, 6.75));
         this.tabela.add(new RendimentoEscolar(alunos.get(0), turmas.get(0), 8.0, 6.0, trabs.get(2), 8.0, 7.5));
-        this.tabela.add(new RendimentoEscolar(alunos.get(0), turmas.get(0), 9.0, 6.0, trabs.get(3), 9.0, 8.0));
+        this.tabela.add(new RendimentoEscolar(alunos.get(0), turmas.get(0), 9.0, 6.0, trabs.get(3), 9.0, 8.));
+
+        this.tabela.add(new RendimentoEscolar(alunos.get(1), turmas.get(0), 6.0, 6.0, trabs.get(0), 6.0, 6.0));
+        this.tabela.add(new RendimentoEscolar(alunos.get(1), turmas.get(0), 7.0, 6.0, trabs.get(1), 7.0, 6.75));
+        this.tabela.add(new RendimentoEscolar(alunos.get(1), turmas.get(0), 8.0, 6.0, trabs.get(2), 8.0, 7.5));
+        this.tabela.add(new RendimentoEscolar(alunos.get(1), turmas.get(0), 9.0, 6.0, trabs.get(3), 9.0, 8.0));
+
+        this.tabela.add(new RendimentoEscolar(alunos.get(2), turmas.get(0), 6.0, 6.0, trabs.get(0), 6.0, 6.0));
+        this.tabela.add(new RendimentoEscolar(alunos.get(2), turmas.get(0), 7.0, 6.0, trabs.get(1), 7.0, 6.75));
+        this.tabela.add(new RendimentoEscolar(alunos.get(2), turmas.get(0), 8.0, 6.0, trabs.get(2), 8.0, 7.5));
+        this.tabela.add(new RendimentoEscolar(alunos.get(2), turmas.get(0), 9.0, 6.0, trabs.get(3), 9.0, 8.0));
 
 
-        this.tabela.add(new RendimentoEscolar(alunos.get(1), turmas.get(1), 6.0, 7.0, trabs.get(4), 5.0, 6.0));
-        this.tabela.add(new RendimentoEscolar(alunos.get(1), turmas.get(1), 7.0, 6.0, trabs.get(5), 8.0, 7.0));
-        this.tabela.add(new RendimentoEscolar(alunos.get(1), turmas.get(1), 8.0, 6.0, trabs.get(6), 8.0, 7.5));
-        this.tabela.add(new RendimentoEscolar(alunos.get(1), turmas.get(1), 9.0, 6.0, trabs.get(7), 10.0, 8.5));
 
 
-        this.tabela.add(new RendimentoEscolar(alunos.get(2), turmas.get(2), 6.0, 8.0, trabs.get(8), 4.0, 5.5));
-        this.tabela.add(new RendimentoEscolar(alunos.get(2), turmas.get(2), 7.0, 6.0, trabs.get(9), 2.0, 5.0));
-        this.tabela.add(new RendimentoEscolar(alunos.get(2), turmas.get(2), 8.0, 6.0, trabs.get(10), 8.0, 7.5));
-        this.tabela.add(new RendimentoEscolar(alunos.get(2), turmas.get(2), 9.0, 6.0, trabs.get(11), 7.0, 7.5));
+
+        this.tabela.add(new RendimentoEscolar(alunos.get(3), turmas.get(1), 6.0, 7.0, trabs.get(4), 5.0, 5.75));
+        this.tabela.add(new RendimentoEscolar(alunos.get(3), turmas.get(1), 7.0, 6.0, trabs.get(5), 8.0, 7.0));
+        this.tabela.add(new RendimentoEscolar(alunos.get(3), turmas.get(1), 8.0, 6.0, trabs.get(6), 8.0, 7.5));
+        this.tabela.add(new RendimentoEscolar(alunos.get(3), turmas.get(1), 9.0, 6.0, trabs.get(7), 10.0, 8.75));
+
+        this.tabela.add(new RendimentoEscolar(alunos.get(4), turmas.get(1), 6.0, 7.0, trabs.get(4), 5.0, 5.75));
+        this.tabela.add(new RendimentoEscolar(alunos.get(4), turmas.get(1), 7.0, 6.0, trabs.get(5), 8.0, 7.0));
+        this.tabela.add(new RendimentoEscolar(alunos.get(4), turmas.get(1), 8.0, 6.0, trabs.get(6), 8.0, 7.5));
+        this.tabela.add(new RendimentoEscolar(alunos.get(4), turmas.get(1), 9.0, 6.0, trabs.get(7), 10.0, 8.75));
+
+        this.tabela.add(new RendimentoEscolar(alunos.get(5), turmas.get(1), 6.0, 7.0, trabs.get(4), 5.0, 5.75));
+        this.tabela.add(new RendimentoEscolar(alunos.get(5), turmas.get(1), 7.0, 6.0, trabs.get(5), 8.0, 7.0));
+        this.tabela.add(new RendimentoEscolar(alunos.get(5), turmas.get(1), 8.0, 6.0, trabs.get(6), 8.0, 7.5));
+        this.tabela.add(new RendimentoEscolar(alunos.get(5), turmas.get(1), 9.0, 6.0, trabs.get(7), 10.0, 8.75));
+
+
+
+
+
+        this.tabela.add(new RendimentoEscolar(alunos.get(6), turmas.get(2), 6.0, 8.0, trabs.get(8), 4.0, 5.5));
+        this.tabela.add(new RendimentoEscolar(alunos.get(6), turmas.get(2), 7.0, 6.0, trabs.get(9), 2.0, 4.0));
+        this.tabela.add(new RendimentoEscolar(alunos.get(6), turmas.get(2), 8.0, 6.0, trabs.get(10), 8.0, 7.5));
+        this.tabela.add(new RendimentoEscolar(alunos.get(6), turmas.get(2), 9.0, 6.0, trabs.get(11), 7.0, 7.5));
+
+        this.tabela.add(new RendimentoEscolar(alunos.get(7), turmas.get(2), 6.0, 8.0, trabs.get(8), 4.0, 5.5));
+        this.tabela.add(new RendimentoEscolar(alunos.get(7), turmas.get(2), 7.0, 6.0, trabs.get(9), 2.0, 4.0));
+        this.tabela.add(new RendimentoEscolar(alunos.get(7), turmas.get(2), 8.0, 6.0, trabs.get(10), 8.0, 7.5));
+        this.tabela.add(new RendimentoEscolar(alunos.get(7), turmas.get(2), 9.0, 6.0, trabs.get(11), 7.0, 7.5));
+
+        this.tabela.add(new RendimentoEscolar(alunos.get(8), turmas.get(2), 6.0, 8.0, trabs.get(8), 4.0, 5.5));
+        this.tabela.add(new RendimentoEscolar(alunos.get(8), turmas.get(2), 7.0, 6.0, trabs.get(9), 2.0, 4.0));
+        this.tabela.add(new RendimentoEscolar(alunos.get(8), turmas.get(2), 8.0, 6.0, trabs.get(10), 8.0, 7.5));
+        this.tabela.add(new RendimentoEscolar(alunos.get(8), turmas.get(2), 9.0, 6.0, trabs.get(11), 7.0, 7.5));
+
+
+
 
         
-        this.tabela.add(new RendimentoEscolar(alunos.get(3), turmas.get(3), 6.0, 9.0, trabs.get(12), 10.0, 8.5));
-        this.tabela.add(new RendimentoEscolar(alunos.get(3), turmas.get(3), 7.0, 6.0, trabs.get(13), 6.0, 6.5));
-        this.tabela.add(new RendimentoEscolar(alunos.get(3), turmas.get(3), 8.0, 6.0, trabs.get(14), 8.0, 7.5));
-        this.tabela.add(new RendimentoEscolar(alunos.get(3), turmas.get(3), 9.0, 6.0, trabs.get(15), 10.0, 8.5));
+        this.tabela.add(new RendimentoEscolar(alunos.get(9), turmas.get(3), 6.0, 9.0, trabs.get(12), 10.0, 8.75));
+        this.tabela.add(new RendimentoEscolar(alunos.get(9), turmas.get(3), 7.0, 6.0, trabs.get(13), 6.0, 6.0));
+        this.tabela.add(new RendimentoEscolar(alunos.get(9), turmas.get(3), 8.0, 6.0, trabs.get(14), 8.0, 7.5));
+        this.tabela.add(new RendimentoEscolar(alunos.get(9), turmas.get(3), 9.0, 6.0, trabs.get(15), 10.0, 8.75));
         
+        this.tabela.add(new RendimentoEscolar(alunos.get(10), turmas.get(3), 6.0, 9.0, trabs.get(12), 10.0, 8.75));
+        this.tabela.add(new RendimentoEscolar(alunos.get(10), turmas.get(3), 7.0, 6.0, trabs.get(13), 6.0, 6.0));
+        this.tabela.add(new RendimentoEscolar(alunos.get(10), turmas.get(3), 8.0, 6.0, trabs.get(14), 8.0, 7.5));
+        this.tabela.add(new RendimentoEscolar(alunos.get(10), turmas.get(3), 9.0, 6.0, trabs.get(15), 10.0, 8.75));
+
+        this.tabela.add(new RendimentoEscolar(alunos.get(11), turmas.get(3), 6.0, 9.0, trabs.get(12), 10.0, 8.75));
+        this.tabela.add(new RendimentoEscolar(alunos.get(11), turmas.get(3), 7.0, 6.0, trabs.get(13), 6.0, 6.0));
+        this.tabela.add(new RendimentoEscolar(alunos.get(11), turmas.get(3), 8.0, 6.0, trabs.get(14), 8.0, 7.5));
+        this.tabela.add(new RendimentoEscolar(alunos.get(11), turmas.get(3), 9.0, 6.0, trabs.get(15), 10.0, 8.75));
 
     }
 
@@ -56,3 +104,4 @@ public class RendimentoEscolarFakeDB extends BaseGenericaFakeDB<RendimentoEscola
         super();
     }
 }
+
